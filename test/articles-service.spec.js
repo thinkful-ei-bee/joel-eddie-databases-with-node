fd16e83b57becfad4944477c3ecb2a03ef1a3d6f
joelcorey <joelcorey@fastmail.com>
2019-04-09 12:14:11 -0700

    finish test

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -39,7 +39,7 @@ describe(`Articles service object`, function() {
   after(() => db.destroy())
 
   context(`Given 'blogful_articles' has data`, () => {
-    before(() => {
+    beforeEach(() => {
       return db
         .into('blogful_articles')
         .insert(testArticles)
@@ -51,6 +51,49 @@ describe(`Articles service object`, function() {
         expect(actual).to.eql(testArticles)
       })
     })
+
+    it(`getById() resolves an article by id from 'blogful_articles' table`, () => {
+      const thirdId = 3
+      const thirdTestArticle = testArticles[thirdId - 1]
+      return ArticlesService.getById(db, thirdId)
+        .then(actual => {
+          expect(actual).to.eql({
+            id: thirdId,
+            title: thirdTestArticle.title,
+            content: thirdTestArticle.content,
+            date_published: thirdTestArticle.date_published,
+          })
+        })
+    })
+
+    it(`deleteArticle() removes an article by id from 'blogful_articles' table`, () => {
+      const articleId = 3
+      return ArticlesService.deleteArticle(db, articleId)
+        .then(() => ArticlesService.getAllArticles(db))
+        .then(allArticles => {
+          // copy the test articles array without the "deleted" article
+          const expected = testArticles.filter(article => article.id !== articleId)
+          expect(allArticles).to.eql(expected)
+        })
+    })
+
+    it(`updateArticle() updates an article from the 'blogful_articles' table`, () => {
+      const idOfArticleToUpdate = 3
+      const newArticleData = {
+        title: 'updated title',
+        content: 'updated content',
+        date_published: new Date(),
+      }
+      return ArticlesService.updateArticle(db, idOfArticleToUpdate, newArticleData)
+        .then(() => ArticlesService.getById(db, idOfArticleToUpdate))
+        .then(article => {
+          expect(article).to.eql({
+            id: idOfArticleToUpdate,
+            ...newArticleData,
+          })
+        })
+    })
+
   })
 
   context(`Given 'blogful_articles' has no data`, () => {
@@ -60,6 +103,24 @@ describe(`Articles service object`, function() {
           expect(actual).to.eql([])
         })
     })
+
+    it(`insertArticle() inserts an article and resolves the article with an 'id'`, () => {
+      const newArticle = {
+        title: 'Test new title',
+        content: 'Test new content',
+        date_published: new Date('2020-01-01T00:00:00.000Z'),
+      }
+      return ArticlesService.insertArticle(db, newArticle)
+       .then(actual => {
+          expect(actual).to.eql({
+            id: 1,
+            title: newArticle.title,
+            content: newArticle.content,
+            date_published: new Date(newArticle.date_published),
+          })
+       })
+    })
+
   })
 
 
