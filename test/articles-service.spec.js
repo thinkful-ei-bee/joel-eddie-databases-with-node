b8888ac4e2cfb6a4b7719ea0fe2008179d67642f
joelcorey <joelcorey@fastmail.com>
2019-04-09 12:01:29 -0700

    empty array tset

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -34,20 +34,33 @@ describe(`Articles service object`, function() {
 
   before(() => db('blogful_articles').truncate())
 
-  before(() => {
-    return db
-      .into('blogful_articles')
-      .insert(testArticles)
-  })
+  afterEach(() => db('blogful_articles').truncate())
     
   after(() => db.destroy())
 
-  describe(`getAllArticles()`, () => {
-    it(`resolves all articles from 'blogful_articles' table`, () => {
+  context(`Given 'blogful_articles' has data`, () => {
+    before(() => {
+      return db
+        .into('blogful_articles')
+        .insert(testArticles)
+    })
+
+    it(`getAllArticles() resolves all articles from 'blogful_articles' table`, () => {
       return ArticlesService.getAllArticles(db)
       .then(actual => {
         expect(actual).to.eql(testArticles)
       })
     })
   })
+
+  context(`Given 'blogful_articles' has no data`, () => {
+    it(`getAllArticles() resolves an empty array`, () => {
+      return ArticlesService.getAllArticles(db)
+        .then(actual => {
+          expect(actual).to.eql([])
+        })
+    })
+  })
+
+
 })
